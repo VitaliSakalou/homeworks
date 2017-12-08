c271332a94bd3f7af678870535d89849aa5b68cb
Vitali Sakalou <vitali.sakalou@gmail.com>
2017-12-08 19:34:40 +0300

    ishop second commit

--- ishop/ishop.js
+++ ishop/ishop.js
@@ -14,9 +14,15 @@
         url: React.PropTypes.string.isRequired,
         height: React.PropTypes.number.isRequired,
       })
-    )
-    
-  },
+    ),
+
+  head:React.PropTypes.shape({
+    logo: React.PropTypes.string.isRequired,
+    name: React.PropTypes.string.isRequired,
+    price: React.PropTypes.string.isRequired,
+    count: React.PropTypes.string.isRequired,
+  })
+},
 
   render: function() {
 
@@ -28,12 +34,12 @@
           React.DOM.td({className:'col'},v.count),
         )
       );   
-      
-      var headOfTable = React.DOM.tr({className:'row'},
-          React.DOM.td({className:'col'},this.props.head.logo),
-          React.DOM.td({className:'col'},this.props.head.name),
-          React.DOM.td({className:'col'},this.props.head.price),
-          React.DOM.td({className:'col'},this.props.head.count),
+
+    var headOfTable = React.DOM.tr({className:'row'},
+        React.DOM.td({className:'col'},this.props.head.logo),
+        React.DOM.td({className:'col'},this.props.head.name),
+        React.DOM.td({className:'col'},this.props.head.price),
+        React.DOM.td({className:'col'},this.props.head.count),
       );  
 
     return React.DOM.div( {className:'main'}, 
